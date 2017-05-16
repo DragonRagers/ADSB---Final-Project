524cd6fcbff45168428cb78e00c7dd37ca3b499c
DragonRagers <dragonragers@gmail.com>
2017-05-16 09:08:04 -0400

    Update Abb.java

--- Abb.java
+++ Abb.java
@@ -3,6 +3,10 @@ package net.mrpaul.MB190.finalProject;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 class Abb extends Game {
 	static int counter = 0;
@@ -11,39 +15,42 @@ class Abb extends Game {
 			new Point(5,5),
 			new Point(5,0),
 			new Point(0,0)
-		};
+	};
 	private Player p = new Player(playerPoints, new Point(400,300), 180);
-  public Abb() {
-    super("Abb",800,600);
-    this.setFocusable(true);
-	this.requestFocus();
-	this.addKeyListener(p);
-  }
-  
-	public void paint(Graphics brush) {
-	Image background = null;
-	try {
-		background = ImageIO.read(new File("background.jpg"));
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	brush.drawImage(background, 0, 0, null);
+	Image background;
 	
-    	// sample code for printing message for debugging
-    	// counter is incremented and this message printed
-    	// each time the canvas is repainted
-    	counter++;
-    	brush.setColor(Color.white);
-    	brush.drawString("Counter is " + counter,10,10);
-    	
-    	p.move();
-    	p.constrain();
-    	p.paint(brush);
-  }
-  
+	
+	public Abb() {
+		super("Abb",800,600);
+		this.setFocusable(true);
+		this.requestFocus();
+		this.addKeyListener(p);
+		try {
+			background = ImageIO.read(new File("background.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void paint(Graphics brush) {		
+		brush.drawImage(background, 0, 0, null);
+
+
+		// sample code for printing message for debugging
+		// counter is incremented and this message printed
+		// each time the canvas is repainted
+		counter++;
+		brush.setColor(Color.white);
+		brush.drawString("Counter is " + counter,10,10);
+
+		p.move();
+		p.constrain();
+		p.paint(brush);
+	}
+
 	public static void main (String[] args) {
-   		Abb a = new Abb();
+		Abb a = new Abb();
 		a.repaint();
-  }
+	}
 }
